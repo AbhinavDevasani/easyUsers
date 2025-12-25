c849d6286644f2f33f0d54e71bda3d5194e51d76
AbhinavDevasani <abhinavdevasani@gmail.com>
2025-12-25 18:04:51 +0530

    Added focus for search

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useContext } from 'react'
-
+import { useNavigate } from 'react-router'
 import UserContext from '../context/UserContext'
 function SearchFilter() {
     const {users,setFiltered,darkMode}=useContext(UserContext)
-    
+    const navigate=useNavigate()
     const handleSearch = (e) => {
         const val = e.target.value.toLowerCase();
         setFiltered(users.filter(user => user.name.toLowerCase().includes(val)));
   };
-
+  const handleFocus=()=>{
+    navigate("/dashboard")
+  }
   return (
     <div className='border h-[5vh] sm:h-[7vh] flex self-center '>
-      <input onChange={handleSearch} placeholder='Search by name' className={` px-5 py-2 outline-none sm:w-[50vw] w-[40vw] ${darkMode?'text-black':'text-white'}`}/>
+      <input onFocus={handleFocus} onChange={handleSearch} placeholder='Search by name' className={` px-5 py-2 outline-none sm:w-[50vw] w-[40vw] ${darkMode?'text-black':'text-white'}`}/>
     </div>
   )
 }
